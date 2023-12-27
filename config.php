b50a9ed428b5609b2e82f8bb03bfb642a38be8f7
Justin Malonson <justin@lyfeblocnetwork.com>
2023-12-27 07:51:30 -0500

    first commit

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+define('DB_SERVER', 'localhost');
+define('DB_USER', 'malonson_time');
+define('DB_PASS', 'VjY0Csrpf@^!');
+define('DB_NAME', 'malonson_bing');
+define('TIMEZONE', 'America/New_York');
+define('APP_SECRET_KEY', 'gfpPPZNsCUD5TacLYk4jih7Y');
+define('APP_SESSION_NAME', 'SMS_GATEWAY');
+define('PURCHASE_CODE', 'f998cd94-1835-4853-bb50-48f3edcd5cbc');
